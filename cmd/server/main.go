e32e094006e866bae2c8aa836e35e6d10b1d3ff4
Luong Duong <duonglc27092000.com>
2024-06-07 00:10:44 +0700

    Go (3): GIN vs ROUTER

---
+++ cmd/server/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/duonglc27092000/go-ecommerce-backend-api/internal/routers"
+)
+
+func main() {
+	r := routers.NewRouter()
+		r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
---
+++ internal/routers/router.go
@@ -0,0 +1,40 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.Default()
+	v1 := r.Group("v1/2024")
+	{
+		v1.GET("ping", Pong)
+		v1.PUT("ping", Pong)
+		v1.PATCH("ping", Pong)
+		v1.DELETE("ping", Pong)
+		v1.HEAD("ping", Pong)
+		v1.OPTIONS("ping", Pong)
+	}
+
+	v2 := r.Group("v2/2024")
+	{
+		v2.GET("ping", Pong)
+		v2.PUT("ping", Pong)
+		v2.PATCH("ping", Pong)
+		v2.DELETE("ping", Pong)
+		v2.HEAD("ping", Pong)
+		v2.OPTIONS("ping", Pong)
+	}
+	return r
+}
+func Pong(c *gin.Context) {
+	name := c.DefaultQuery("name","LuongDuong")
+	uid := c.Query("uid")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong" + name,
+		"uid": uid,
+		"users": []string{"d1","d2","d3"},
+	})
+}
