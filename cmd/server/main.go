7120137ba8e541556041678d47b431a6a638cdd2
Luong Duong <duonglc27092000.com>
2024-06-13 01:12:54 +0700

    Section 101: Go (7) GIN vs Viper Config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,5 +6,5 @@ import (
 
 func main() {
 	r := routers.NewRouter()
-		r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+		r.Run(":8082") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
