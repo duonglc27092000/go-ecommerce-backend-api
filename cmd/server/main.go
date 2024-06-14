f09c06715229f4ac115cc807f6073f57b89ef68f
Luong Duong <duonglc27092000.com>
2024-06-14 23:57:52 +0700

    Section 102: Go (8) GIN vs Middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/duonglc27092000/go-ecommerce-backend-api/internal/routers"
 )
-
+func AA
 func main() {
 	r := routers.NewRouter()
 		r.Run(":8082") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
